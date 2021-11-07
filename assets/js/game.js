994dda168d5f88333ab6f468d12126f0dc5ea472
Mary Margaret <mmlawton15@gmail.com>
2021-11-07 18:25:44 -0500

    added values to player and enemy lives and attacks in JS

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,30 +1,38 @@
-
-
-
-
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
+console.log(playerName, playerAttack, playerHealth);
 
-// CONSOLE
-console.log(playerName);
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log("This logs a string, good for leaving yourself a message");
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+  
+    enemyHealth = enemyHealth - playerAttack;
 
-console.log(10 + 10);
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+    if (enemyHealth <=0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
 
-console.log("Our robot's name is " + playerName);
+    playerHealth = playerHealth - enemyAttack;
 
-var name = "your name";
-console.log(name);
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+  
+    // check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    } 
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 
+    };
 
-
-
-
-function fight() {
-    window.alert("The fight has begun!");
-}
-
-
-
-// fight();
+fight();
