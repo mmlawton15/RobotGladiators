250f1c8b15c06546ff5637f5c3103f4d79574610
Mary Margaret <mmlawton15@gmail.com>
2021-11-14 12:45:16 -0500

    fixed the case sensititivity issue to make the game more funcitonal

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,20 +5,33 @@ var randomNumber = function(min, max) {
 };
 
 
-var fight = function(enemy) { 
-  while  (playerInfo.health > 0 && enemy.health > 0)  {
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-
-    if (promptFight === "skip" || promptFight === "SKIP") {
+var fightOrSkip = function() {
+  var promptFight = window.prompt('Would you like to fight or skip this battle? Enter FIGHT or SKIP to choose.');
+    if (promptFight === "" || promptFight ===null) {
+      window.alert("You need to provide a valid answer! Please try again.");
+      return fightOrSkip();
+    }
+    promptFight = promptFight.toLowerCase();
+    if (promptFight ==="skip" || promptFight ==="SKIP") {
       var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
     if (confirmSkip) {
-    window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
-      playerInfo.money = Math.max(0, playerInfo.money - 10);
-      console.log("playerMoney", playerInfo.money)
-      break;
+      window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+      playerInfo.playerMoney = playerInfo.money - 10;
+      shop();
+      return true;
     }
   }
+  return false;
+}
+
+
+var fight = function(enemy) { 
+  while  (playerInfo.health > 0 && enemy.health > 0)  {
+    if (fightOrSkip()){
+      break;
+    }
+
+    var damage = randomNummber(playerInfo.Attack - 3, playerInfo.attack);
 
   // generate random damage value based on player's attack power
   var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
